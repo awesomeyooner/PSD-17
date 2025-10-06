d82c6794b9b5d7659c9a54f2b6c4a33aa2d16b60
Susan Yoon <awesomeyooner@gmail.com>
2025-10-05 23:30:45 -0700

    added new status code and added user input methods

--- i2c_test/project/include/util/util.hpp
+++ i2c_test/project/include/util/util.hpp
@@ -3,9 +3,122 @@
 
 #include <string>
 #include <iostream>
+#include "status.hpp"
 
-void print(std::string text){
-    std::cout << text << std::endl;
-}
+namespace util{
+
+    void print(std::string text){
+        std::cout << text << std::endl;
+    }
+
+    /**
+     * @brief Gets user input as a string
+     * 
+     * @return `StatusedValue<std::string>` Returns `StatusCode::ERROR` when the message is `exit`, `StatusCode::OK` otherwise. 
+     */
+    StatusedValue<std::string> get_user_input_string(){
+        std::string buffer;
+        StatusCode status;
+
+        std::cout << "\nSay something!" << std::endl;
+
+        std::getline(std::cin, buffer);
+
+        if(buffer == "exit")
+            status = StatusCode::ERROR;
+        else
+            status = StatusCode::OK;
+
+        return StatusedValue<std::string>(buffer, status);
+    }
+
+    /**
+     * @brief Gets user input as a double
+     * 
+     * @return `StatusedValue<double>` Returns `StatusCode::ERROR` when the message is `exit`, `StatusCode::FAILED` if string failed to parse, `StatusCode::OK` if string properly parsed
+     */
+    StatusedValue<double> get_user_input_double(){
+        std::string buffer;
+        double value;
+        StatusCode status = StatusCode::OK;
+
+        std::cout << "\nSay something!" << std::endl;
+
+        std::getline(std::cin, buffer);
+
+        if (buffer == "exit"){
+            status = StatusCode::ERROR;
+            return StatusedValue<double>(value, status);
+        }
+
+        try{
+            value = std::stod(buffer);
+        }
+        catch(std::exception e){
+            status = StatusCode::FAILED;
+        }
+
+        return StatusedValue<double>(value, status);
+    }
+
+    /**
+     * @brief Gets user input as a float
+     * 
+     * @return `StatusedValue<float>` Returns `StatusCode::ERROR` when the message is `exit`, `StatusCode::FAILED` if string failed to parse, `StatusCode::OK` if string properly parsed
+     */
+    StatusedValue<float> get_user_input_float(){
+        std::string buffer;
+        float value;
+        StatusCode status = StatusCode::OK;
+
+        std::cout << "\nSay something!" << std::endl;
+
+        std::getline(std::cin, buffer);
+
+        if (buffer == "exit"){
+            status = StatusCode::ERROR;
+            return StatusedValue<float>(value, status);
+        }
+
+        try{
+            value = std::stof(buffer);
+        }
+        catch(std::exception e){
+            status = StatusCode::FAILED;
+        }
+
+        return StatusedValue<float>(value, status);
+    }
+
+    /**
+     * @brief Gets user input as an int
+     * 
+     * @return `StatusedValue<int>` Returns `StatusCode::ERROR` when the message is `exit`, `StatusCode::FAILED` if string failed to parse, `StatusCode::OK` if string properly parsed
+     */
+    StatusedValue<int> get_user_input_int(){
+        std::string buffer;
+        int value;
+        StatusCode status = StatusCode::OK;
+
+        std::cout << "\nSay something!" << std::endl;
+
+        std::getline(std::cin, buffer);
+
+        if (buffer == "exit"){
+            status = StatusCode::ERROR;
+            return StatusedValue<int>(value, status);
+        }
+
+        try{
+            value = std::stoi(buffer);
+        }
+        catch(std::exception e){
+            status = StatusCode::FAILED;
+        }
+
+        return StatusedValue<int>(value, status);
+    }
+
+} // namespace util
 
 #endif // UTIL_HPP
