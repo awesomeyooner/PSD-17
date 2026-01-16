2f74fea8ca74fa41bc25b79524db2a7d4f047416
Susan Yoon <awesomeyooner@gmail.com>
2026-01-15 22:26:18 -0800

    changed defines to constexprs

--- stm32f446re_testbench/include/devices/as5047.hpp
+++ stm32f446re_testbench/include/devices/as5047.hpp
@@ -1,3 +1,5 @@
+// https://look.ams-osram.com/m/d05ee39221f9857/original/AS5047P-DS000324.pdf
+
 #ifndef AS5047_HPP
 #define AS5047_HPP
 
@@ -5,13 +7,13 @@
 #include "SPI.h"
 
 
-#define CPR 16384 // 14 bits
-#define UNCOMPENSATED_ANGLE_REGISTER 0x3FFE
-#define COMPENSATED_ANGLE_REGISTER 0x3FFF
-#define MAGNETIC_MAGNITUDE_REGISTER 0x3FFD
-#define PARITY 0x8000
-#define READ_WRITE 0x4000
-#define RESULT_MASK 0x3FFF
+// #define CPR 16384 // 14 bits
+// #define UNCOMPENSATED_ANGLE_REGISTER 0x3FFE
+// #define COMPENSATED_ANGLE_REGISTER 0x3FFF
+// #define MAGNETIC_MAGNITUDE_REGISTER 0x3FFD
+// #define PARITY 0x8000
+// #define READ_WRITE 0x4000
+// #define RESULT_MASK 0x3FFF
 
 class AS5047
 {
@@ -41,13 +43,13 @@ class AS5047
         uint16_t NOP();
         uint16_t spi_transfer16(uint16_t write_data);
 
-        // static constexpr int CPR = 16384; // 14 bits
-        // static constexpr uint16_t UNCOMPENSATED_ANGLE_REGISTER = 0x3FFE;
-        // static constexpr uint16_t COMPENSATED_ANGLE_REGISTER = 0x3FFF;
-        // static constexpr uint16_t MAGNETIC_MAGNITUDE_REGISTER = 0x3FFD;
-        // static constexpr uint16_t PARITY = 0x8000;
-        // static constexpr uint16_t READ_WRITE = 0x4000;
-        // static constexpr uint16_t RESULT_MASK = 0x3FFF;
+        static constexpr int CPR = 16384; // 14 bits
+        static constexpr uint16_t UNCOMPENSATED_ANGLE_REGISTER = 0x3FFE;
+        static constexpr uint16_t COMPENSATED_ANGLE_REGISTER = 0x3FFF;
+        static constexpr uint16_t MAGNETIC_MAGNITUDE_REGISTER = 0x3FFD;
+        static constexpr uint16_t PARITY = 0x8000;
+        static constexpr uint16_t READ_WRITE = 0x4000;
+        static constexpr uint16_t RESULT_MASK = 0x3FFF;
 
         static constexpr SPISettings SETTINGS = SPISettings(8000000, BitOrder::MSBFIRST, SPIMode::SPI_MODE1);
 
