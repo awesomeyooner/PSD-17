a2659ee8673fb530e72e3513ebcbeb47c3f4607b
Aaron Yoon <awesomeyooner@gmail.com>
2026-08-07 17:51:52 -0700

    added plib back

--- serial_test/project/src/main.cpp
+++ serial_test/project/src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 #include "plib/util/system.hpp"
+#include "plib/util/logger.hpp"
+
+#include "CommiFaceLib/protocols/serial.hpp"
 
 int main(int argc, char* argv[])
 {
+
+    SerialInterface serial;
+
+    serial.init_field("product", "STM32 Virtual ComPort");
+    
 
     return 0;
 
