fc09675f6ca36598c71807a5773e53d92182d118
Aaron Yoon <awesomeyooner@gmail.com>
2026-08-08 00:36:52 -0700

    Updated serial_test to read sensor data and send voltages

--- serial_test/project/src/main.cpp
+++ serial_test/project/src/main.cpp
@@ -3,15 +3,54 @@
 #include "plib/util/system.hpp"
 #include "plib/util/logger.hpp"
 
+#include "plib/util/string_util.hpp"
+
 #include "CommiFaceLib/protocols/serial.hpp"
+#include "CommiFaceLib/interfaces/communication_interface.hpp"
+
+#include "PlotLib/gnu_plotter.hpp"
+#include "PlotLib/implot_plotter.hpp"
+
+#include "plib/util/util.hpp"
+#include "plib/util/system.hpp"
 
 int main(int argc, char* argv[])
 {
+    ImPlotter::initialize();
 
     SerialInterface serial;
 
     serial.init_field("product", "STM32 Virtual ComPort");
     
+    while(System::is_alive())
+    {
+        auto angle_read = serial.request_data<double>(101, 500);
+
+        if(!angle_read.is_OK())
+        {
+            Logger::error("Failed to read! Skipping iteration...");
+            continue;
+        }
+
+        double voltage = 5;
+
+        serial.write_data<double>(100, voltage);
+
+        Logger::debug(voltage);
+
+        ImPlotter::push_data(
+            angle_read.value,
+            "AS5047 Angle (Radians)"
+        );
+
+        if(ImPlotter::update() == StatusCode::FAILED)
+            System::shutdown();
+
+        // Logger::info(angle_read.value);
+    }
+
+    serial.close();
+    ImPlotter::shutdown();
 
     return 0;
 
---
+++ serial_test/project/test/test.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdio>
+#include <type_traits>
+#include <thread>
+
+#include "plib/util/system.hpp"
+
+#include "PlotLib/implot_plotter.hpp"
+
+
+using namespace status_utils;
+
+
+void test_plotter()
+{
+    // Initialize Plotter
+    ImPlotter::initialize();
+
+    // Loop until user requests to quit
+    while (System::is_alive())
+    {
+        // Sample Data
+        ImPlotter::push_data(sin(System::get_epoch()), "My First Line Plot");
+        ImPlotter::push_data(cos(System::get_epoch()), "My Second Line Plot");
+
+        // If the user requests to quit, then shutdown
+        if (ImPlotter::update() == status_utils::StatusCode::FAILED)
+            System::shutdown();
+    }
+
+    // Shutdown Plotter
+    ImPlotter::shutdown();
+}
+
+
+void run_user_input()
+{
+    std::thread(
+        []()
+        {
+            while(System::is_alive())
+            {
+                StatusedValue<std::string> input = util::get_user_input_string("Type exit to quit the program >> ");
+
+                if(!input.is_OK())
+                {
+                    System::shutdown();
+                    break;
+                }
+            }
+
+            Logger::info("Shutting down...");
+        }
+    ).detach();
+}
+
+int main(int argc, char* argv[])
+{
+    // run_user_input();
+    test_plotter();
+
+    return 0;
+}
