2b3c211a27624a4e496b411deb31f3cc685b7ad1
Aaron Yoon <awesomeyooner@gmail.com>
2026-08-08 20:09:26 -0700

    updated plotlib

--- serial_test/project/src/main.cpp
+++ serial_test/project/src/main.cpp
@@ -16,7 +16,7 @@
 
 int main(int argc, char* argv[])
 {
-    ImPlotter::initialize();
+    ImPlotter::init();
 
     SerialInterface serial;
 
--- serial_test/project/test/test.cpp
+++ serial_test/project/test/test.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <cstdio>
-#include <type_traits>
-#include <thread>
 
 #include "plib/util/system.hpp"
 
+#include "PlotLib/scrolling_buffer.hpp"
 #include "PlotLib/implot_plotter.hpp"
 
 
 using namespace status_utils;
 
 
-void test_plotter()
+float input = 0;
+float slider = 0;
+
+float input_on_change = 0;
+
+
+int main(int argc, char* argv[])
 {
     // Initialize Plotter
-    ImPlotter::initialize();
+    ImPlotter::init();
 
     // Loop until user requests to quit
     while (System::is_alive())
@@ -23,41 +28,26 @@ void test_plotter()
         ImPlotter::push_data(sin(System::get_epoch()), "My First Line Plot");
         ImPlotter::push_data(cos(System::get_epoch()), "My Second Line Plot");
 
+        // Add additional inputs
+        std::function<void()> add_inputs = []()
+        {
+            ImGui::InputFloat("My Input", &input, 0.1f, 1.0f, "%.3f s");
+            ImGui::SliderFloat("My Slider", &slider, 0, 100, "%.3f units");
+
+            // Run code upon pressing enter
+            if(ImGui::InputFloat("My Input Trigger", &input, 0.1f, 1.0f, "%.3f s", ImGuiInputTextFlags_EnterReturnsTrue))
+            {
+                std::cout << "Input Changed!" << std::endl;
+            }
+        };
+
         // If the user requests to quit, then shutdown
-        if (ImPlotter::update() == status_utils::StatusCode::FAILED)
+        if (ImPlotter::update(add_inputs) == status_utils::StatusCode::FAILED)
             System::shutdown();
     }
 
     // Shutdown Plotter
     ImPlotter::shutdown();
-}
-
-
-void run_user_input()
-{
-    std::thread(
-        []()
-        {
-            while(System::is_alive())
-            {
-                StatusedValue<std::string> input = util::get_user_input_string("Type exit to quit the program >> ");
-
-                if(!input.is_OK())
-                {
-                    System::shutdown();
-                    break;
-                }
-            }
-
-            Logger::info("Shutting down...");
-        }
-    ).detach();
-}
-
-int main(int argc, char* argv[])
-{
-    // run_user_input();
-    test_plotter();
 
     return 0;
 }
