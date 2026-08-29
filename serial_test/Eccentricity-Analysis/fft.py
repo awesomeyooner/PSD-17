e2842077d227e623395a7520638b0b988f4603cf
Aaron Yoon <awesomeyooner@gmail.com>
2026-08-28 18:33:45 -0700

    updated curve fitting

--- serial_test/Eccentricity-Analysis/fft.py
+++ serial_test/Eccentricity-Analysis/fft.py
@@ -4,13 +4,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 
-DATA_FILE = "data6"
+DATA_FILE = "data8"
 FILE_EXT = ".csv"
 
 data = pd.read_csv(DATA_FILE + FILE_EXT)
 
 time = data['Normalized Time'].to_numpy()
 residual = data['Residual'].to_numpy()
+readings = data['Encoder Angle (Radians)'].to_numpy()
 
 num_samples = len(time)
 total_time = time[-1] - time[0]
@@ -23,29 +24,25 @@ frequencies = np.fft.rfftfreq(num_samples, d=period)
 phases = np.angle(fft_output)
 
 amplitude = np.abs(fft_output) / num_samples
-amplitude[1:] = amplitude[1:] * 2
 
-# Output Amplitude and Frequency to CSV
-# output_data = pd.DataFrame({
-#     'frequency_hz': frequencies,
-#     'amplitude': amplitude
-# })
+if num_samples % 2 == 0:
+    amplitude[1:-1] *= 2
+else:
+    amplitude[1:] *= 2
 
-# output_data.to_csv("fft_results.csv", index=False)
-
-peak_idxs, properties = find_peaks(amplitude, threshold=0.0001)
+peak_idxs, properties = find_peaks(amplitude, threshold=0.000075)
 
 frequency_peaks = []
 amplitude_peaks = []
 phase_peaks = []
 
 for peak_idx in peak_idxs:
-    x = frequencies[peak_idx]
-    y = amplitude[peak_idx]
+    f = frequencies[peak_idx]
+    amp = amplitude[peak_idx]
     phase = phases[peak_idx]
 
-    frequency_peaks.append(x)
-    amplitude_peaks.append(y)
+    frequency_peaks.append(f)
+    amplitude_peaks.append(amp)
     phase_peaks.append(phase)
 
 peak_csv = pd.DataFrame({
@@ -59,42 +56,64 @@ print("Frequency data saved!")
 constructed_x = []
 constructed_y = []
 
-for t in time:
+for t_i in range(len(time)):
+
+    t = time[t_i]
+    theta = readings[t_i]
 
     constructed_x.append(t)
 
     sum = 0
 
     for p_i in range(len(peak_idxs)):
+        # n = pow(2, p_i)
         f = frequency_peaks[p_i]
+        n = round(2 * math.pi * f)
         amp = amplitude_peaks[p_i]
         phase = phase_peaks[p_i]
 
-        sum += amp * math.sin(2 * math.pi * f * t + phase)
+        phase_abs = phase - n * readings[0]
+
+        # sum += amp * math.sin(n * theta + phase_abs)
+        sum += amp * math.cos(n * (theta - readings[0] + time[0]) + (phase))
+        # sum -= amp * math.sin(2 * math.pi * f * (theta - residual[0]) + phase)
+        # sum += amp * math.sin(2 * math.pi * f * (t - time[0]) + phase)
 
     constructed_y.append(sum)
 
+diff_y = []
 
-fig, axs = plt.subplots(1, 2, figsize=(10, 4))
+for i in range(len(time)):
+    raw = residual[i]
+    predict = constructed_y[i]
 
-axs[0].plot(frequencies, amplitude, color='blue', linewidth=1.5)
+    diff_y.append(raw - predict)
+
+
+fig1, ax1 = plt.subplots(figsize=(10, 4))
+
+ax1.plot(frequencies, amplitude, color='blue', linewidth=1.5)
 
 for peak_idx in peak_idxs:
     x = frequencies[peak_idx]
     y = amplitude[peak_idx]
 
-    axs[0].plot(x, y, "x")
+    ax1.plot(x, y, "x")
 
-axs[0].set_title("Frequency Spectrum")
-axs[0].set_xlabel("Frequency (Hz)")
-axs[0].set_ylabel("Amplitude")
-axs[0].grid(True, linestyle='--', alpha=0.6)
+ax1.set_title("Frequency Spectrum")
+ax1.set_xlabel("Frequency (Hz)")
+ax1.set_ylabel("Amplitude")
+ax1.grid(True, linestyle='--', alpha=0.6)
 
-axs[1].plot(constructed_x, constructed_y, color='red', linewidth=1.5)
-axs[1].set_title("Reconstructed Noise")
-axs[1].set_xlabel("Time (seconds)")
-axs[1].set_ylabel("Angle (rads)")
-axs[1].grid(True, linestyle='--', alpha=0.6)
+fig2, ax2 = plt.subplots(figsize=(10, 4))
+
+# ax2.plot(constructed_x, constructed_y, color='red', linewidth=1.5)
+# ax2.plot(time, residual, color='green', linewidth=1.5)
+ax2.plot(time, diff_y, color='purple', linewidth=1.5)
+ax2.set_title("Reconstructed Noise")
+ax2.set_xlabel("Time (seconds)")
+ax2.set_ylabel("Angle (rads)")
+ax2.grid(True, linestyle='--', alpha=0.6)
 
 plt.tight_layout()
 plt.show()
